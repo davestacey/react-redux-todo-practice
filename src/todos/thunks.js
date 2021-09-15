3c00f4997bdaef2d95a228d94ddfdc7414e6f33a
David <davestacey@users.noreply.github.com>
2021-09-15 17:37:03 -0400

    refactor addTodo for new api call

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -22,7 +22,12 @@ export const loadTodos = () => async (dispatch, getState) => {
 
 export const addTodoRequest = title => async dispatch => {
   try {
-    const body = JSON.stringify({ title });
+    const bodyObj = {
+      "title": title,
+      "body": "'bar'",
+      "userId": "1",
+    }
+    const body = JSON.stringify({ bodyObj });
     const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
       headers: {
         'Content-Type': 'application/json',
@@ -30,13 +35,16 @@ export const addTodoRequest = title => async dispatch => {
       method: 'post',
       body,
     });
-    const todo = await response.json();
-
-    todo.map(todo => {
-      return { ...todo, idRandom: Math.random().toString(36).substr(2, 9) };
-    });
-
-    dispatch(createTodo(todo));
+    const todoResponse = await response.json();
+    const newTodo = {
+      userId: 6,
+      id: todoResponse.id + 700,
+      title: todoResponse.bodyObj.title,
+      completed: false,
+      idRandom: Math.random().toString(36).substr(2, 9),
+    }
+    newTodo.idRandom = Math.random().toString(36).substr(2, 9)
+    dispatch(createTodo(newTodo));
   } catch (e) {
     dispatch(displayAlert(e));
   }
